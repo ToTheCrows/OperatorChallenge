e4f6c02b95b6d4667fc24ce24dd81846becfc119
Raphael Tango05 <raphael.tango05@gmail.com>
2022-11-13 21:18:11 +0100

    finish first challenge

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+        /*
+        wrong because instead of using a logical operator I used
+        and assignment operator -> need == for if
+        int newValue = 50;
+        if (newValue = 50) {
+            execute code
+        }
+         */
+        int newValue = 50;
+        if(newValue == 50) {
+            // execute Code
+        }
+   }
 }
